b3b4845d4e1c0870170110b1a71d853dfa8bcc66
fitzgebt <fitzgebt@gmail.com>
2021-09-01 09:50:46 -0700

    commit tree e57a8443b971f1ba4022bcd167aa28d856fad359

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
+let state = { friends: [] }
+let action =  {
+    type: "ADD_FRIEND",
+    friend: {
+        name: "Chrome Boi",
+        hometown: "NYC",
+        id: 1
+    }
+}
+
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return { friends: [...state.friends, action.friend] };
+        case "REMOVE_FRIEND":
+            const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+        return (
+            {...state,
+                friends: [
+                ...state.friends.slice(0, removalIndex),
+                ...state.friends.slice(removalIndex + 1)
+                ]
+            }
+        )
+        default:
+            return state
+    }
 }
